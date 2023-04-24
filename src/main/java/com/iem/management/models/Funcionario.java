709c3d5dc2564444085bf90d4d465a742bf4d0da
oligregz <gregorysak90@gmail.com>
2023-04-24 10:26:51 -0300

    feat: employee model implemented

--- src/main/java/com/iem/management/models/Funcionario.java
+++ src/main/java/com/iem/management/models/Funcionario.java
@@ -1,5 +1,34 @@
 package com.iem.management.models;
 
-public class Funcionario {
+import java.math.BigDecimal;
+
+public class Funcionario extends Pessoa {
+
+  private BigDecimal salario;
+  
+  private String funcao;
+
+  public Funcionario() {}
+
+  public Funcionario(BigDecimal salario, String funcao) {
+    this.salario = salario;
+    this.funcao = funcao;
+  }
+
+  public BigDecimal getSalario() {
+    return salario;
+  }
+
+  public void setSalario(BigDecimal salario) {
+    this.salario = salario;
+  }
+
+  public String getFuncao() {
+    return funcao;
+  }
+
+  public void setFuncao(String funcao) {
+    this.funcao = funcao;
+  }
 
 }

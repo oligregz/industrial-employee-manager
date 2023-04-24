7ef0c98941ee351fa5149ea2ca32cd284edd5ec6
oligregz <gregorysak90@gmail.com>
2023-04-24 10:23:43 -0300

    feat: person model implemented

--- src/main/java/com/iem/management/models/Pessoa.java
+++ src/main/java/com/iem/management/models/Pessoa.java
@@ -1,5 +1,34 @@
 package com.iem.management.models;
 
+import java.time.LocalDate;
+
 public class Pessoa {
+
+  private String nome;
+  
+  private LocalDate dataNascimento;
+
+  public Pessoa() {}
+
+  public Pessoa(String nome, LocalDate dataNascimento) {
+    this.nome = nome;
+    this.dataNascimento = dataNascimento;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public LocalDate getDataNascimento() {
+    return dataNascimento;
+  }
+
+  public void setDataNascimento(LocalDate dataNascimento) {
+    this.dataNascimento = dataNascimento;
+  }
 
 }

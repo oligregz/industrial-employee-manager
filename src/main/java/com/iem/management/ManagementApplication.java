6ca7a69bd927e9f3e724b2966cd56ede8ca13d67
oligregz <gregorysak90@gmail.com>
2023-04-29 17:39:32 -0300

    feat: removidor de joao implementado

--- src/main/java/com/iem/management/ManagementApplication.java
+++ src/main/java/com/iem/management/ManagementApplication.java
@@ -3,18 +3,32 @@ package com.iem.management;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import com.iem.management.models.Funcionario;
 
 public class ManagementApplication {
 
+  static ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
+
 	public static void main(String[] args) {
+	  // 3.1
 	  insereFuncionariosEmOrdem();
+	  // 3.2
+	  removeJoao();
 	}
 
+	public static List<Funcionario> removeJoao() {
+	  funcionarios.clear();
+	  insereFuncionariosEmOrdem();
+	  List<Funcionario> semJoao = funcionarios.stream()
+	  .filter(funcionario -> !funcionario.getNome().equals("João"))
+	  .collect(Collectors.toList());
+	  System.out.println(semJoao);
+	  return semJoao;
+	}
+	
 	public static ArrayList<Funcionario> insereFuncionariosEmOrdem() {
-
-	  ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
-
 	  funcionarios
 	    .add(new Funcionario("Maria", LocalDate.of(2000,10,18), new BigDecimal("2009.44"), "Operador"));
 	  funcionarios
@@ -32,10 +46,11 @@ public class ManagementApplication {
 	  funcionarios
 	    .add(new Funcionario("Laura", LocalDate.of(1994,07,8), new BigDecimal("3017.45"), "Gerente"));
 	  funcionarios
-	    .add(new Funcionario("Heloísa", LocalDate.of(2003,05,24), new BigDecimal("1606.85"), "Eletricista"));
+	    .add(new Funcionario("Heloísa", LocalDate.of(2003,05,24), new BigDecimal("1606.85"),
+	        "Eletricista"));
 	  funcionarios
 	    .add(new Funcionario("Helena", LocalDate.of(1996,9,02), new BigDecimal("2799.93"), "Gerente"));
-	  System.out.println(funcionarios);
+	  System.out.println("3.1: " + funcionarios);
 	  return funcionarios;
 	}
 }
